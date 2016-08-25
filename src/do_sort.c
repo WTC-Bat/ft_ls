9a02a26120c50ffed3ae0b12387532ed87a70c95
Matthew VAN WYK <mvanwyk@e5r6p6.jnb.42.fr>
2016-08-25 15:10:03 +0200

    fixed erroneous sorting

--- src/do_sort.c
+++ src/do_sort.c
@@ -6,59 +6,73 @@
 /*   By: mvanwyk <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/13 15:02:37 by mvanwyk           #+#    #+#             */
-/*   Updated: 2016/08/25 14:48:48 by mvanwyk          ###   ########.fr       */
+/*   Updated: 2016/08/25 15:09:12 by mvanwyk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 
-static void	do_sort2(struct s_file *root, struct s_file *nxt)
-{
-	time_t	tmp_ttmtime;
-	char	*tmp_mtime;
-	char	*tmp_name;
+static void	do_sort3(struct s_file *root, struct s_file *nxt)
+{	
 	int		tmp_isdir;
 	char	*tmp_path;
 
+	tmp_isdir = root->is_dir;
+	tmp_path = root->dir_path;
+	root->is_dir = nxt->is_dir;
+	root->dir_path = nxt->dir_path;
+	nxt->is_dir = tmp_isdir;
+	nxt->dir_path = tmp_path;
+}
+
+static void	do_sort2(struct s_file *root, struct s_file *nxt)
+{
+	size_t	tmp_size;
+	char	*tmp_strsize;
+	time_t	tmp_ttmtime;
+	char	*tmp_mtime;
+	char	*tmp_name;
+
+	tmp_size = root->size;
+	tmp_strsize = root->strsize;
 	tmp_ttmtime = root->ttmtime;
 	tmp_mtime = root->mod_time;
 	tmp_name = root->name;
-	tmp_isdir = root->is_dir;
-	tmp_path = root->dir_path;
+	root->size = nxt->size;
+	root->strsize = nxt->strsize;
 	root->ttmtime = nxt->ttmtime;
 	root->mod_time = nxt->mod_time;
 	root->name = nxt->name;
-	root->is_dir = nxt->is_dir;
-	root->dir_path = nxt->dir_path;
+	nxt->size = tmp_size;
+	nxt->strsize = tmp_strsize;
 	nxt->ttmtime = tmp_ttmtime;
 	nxt->mod_time = tmp_mtime;
 	nxt->name = tmp_name;
-	nxt->is_dir = tmp_isdir;
-	nxt->dir_path = tmp_path;
+	do_sort3(root, nxt);
 }
 
 void		do_sort(struct s_file *root, struct s_file *nxt)
 {
 	char	*tmp_perms;
 	int		tmp_hlinks;
+	char	*tmp_strhlinks;
 	char	*tmp_uname;
 	char	*tmp_gname;
-	size_t	tmp_size;
 
 	tmp_perms = root->perms;
 	tmp_hlinks = root->hlinks;
+	tmp_strhlinks = root->strhlinks;
 	tmp_uname = root->uname;
 	tmp_gname = root->gname;
-	tmp_size = root->size;
 	root->perms = nxt->perms;
 	root->hlinks = nxt->hlinks;
+	root->strhlinks = nxt->strhlinks;
 	root->uname = nxt->uname;
 	root->gname = nxt->gname;
-	root->size = nxt->size;
 	nxt->perms = tmp_perms;
 	nxt->hlinks = tmp_hlinks;
+	nxt->strhlinks = tmp_strhlinks;
 	nxt->uname = tmp_uname;
 	nxt->gname = tmp_gname;
-	nxt->size = tmp_size;
 	do_sort2(root, nxt);
 }
